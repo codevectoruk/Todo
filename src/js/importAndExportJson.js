4a07136f9077d6de2e707b7c58fa484fa9925438
Edward Wright <ed.wright@live.co.uk>
2019-04-20 22:48:56 +0100

    added export json

--- src/js/importAndExportJson.js
+++ src/js/importAndExportJson.js
@@ -1,5 +1,8 @@
-function downloadObjectAsJson(exportObj, exportName){
-   var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+function downloadObjectAsJson(){
+    var exportObj = todo;
+    var today = new Date();
+    var exportName = "todoBackup_" + today.toISOString()
+   var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, 2));
    var downloadAnchorNode = document.createElement('a');
    downloadAnchorNode.setAttribute("href",     dataStr);
    downloadAnchorNode.setAttribute("download", exportName + ".json");
