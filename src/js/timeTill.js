2e96ad1d4372f5289ded51bacab3656a16c319bf
Edward Wright <ed.wright@live.co.uk>
2019-04-22 19:14:26 +0100

    changed due! to overdue

--- src/js/timeTill.js
+++ src/js/timeTill.js
@@ -11,7 +11,7 @@ function timeTill(futureDate) {
     minutes = minutes - (days * 24 * 60) - (hours * 60);
     var output = "";
     if (dateFuture - dateNow <= 0) {
-        output = "Due!";
+        output = "Overdue";
     } else {
         if (days >= 1) {
             if (days == 1) {
